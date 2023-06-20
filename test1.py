edf4a33ff58403b91887a4ce049d09b7e0c600bc
Mukesh <mukeshkumar.6649@gmail.com>
2023-06-20 16:05:17 +0530

    This is test1 file

---
+++ test1.py
@@ -0,0 +1,16 @@
+print("hello world")
+
+a = 10
+if a==10:
+    print("True")
+
+#Multiple assignment
+a=b=c=10
+print(a,end=" ")
+print(b,end=" ")
+print(c)
+
+a=b=c=10
+print(a,b,c,sep=" ")
+# print(b,sep=",")
+# print(c)
